0aae5692a685b059473c31b5ac2dca60fd3f465a
Fatih <fatihgun17.35@gmail.com>
2024-07-21 12:44:09 +0300

    controller extended

--- src/main/java/com/aws/test/demo/controller/HelloController.java
+++ src/main/java/com/aws/test/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/", method = RequestMethod.GET)
+@RequestMapping(method = RequestMethod.GET)
 public class HelloController {
 
     @GetMapping
